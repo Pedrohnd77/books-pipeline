ae2d7670fa97dc9420c8d9e3fc4f358cea54e19b
Pedro Domingos <pedro.hndomingos@gmail.com>
2024-08-06 21:49:36 -0300

    Update web_scrapper.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,19 +1,27 @@
 from web_scraper import coletar_dados, iniciar_driver
 import os
+import csv
 
 def main():
     url_base = 'https://books.toscrape.com/catalogue/page-{}.html'
     driver = iniciar_driver()
 
+    # Validando se a pasta 'data' existe
+    os.makedirs('data', exist_ok=True)
+
     for pagina in range(1, 51):
         url = url_base.format(pagina)
         dados = coletar_dados(driver, url)
         if dados:
-            nome_arquivo = os.path.join('data', f'dados_pagina_{pagina}.txt')
-            with open(nome_arquivo, 'w') as f:
+            nome_arquivo = os.path.join('data', f'dados_pagina_{pagina}.csv')
+            with open(nome_arquivo, 'w', newline='', encoding='utf-8') as csvfile:
+                fieldnames = ['title', 'price', 'stars']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+                writer.writeheader()
                 for item in dados:
-                    f.write("%s\n" % item)
-            print(f"Dados da página {pagina} salvos localmente em {nome_arquivo}")
+                    writer.writerow(item)
+            print(f"Dados da página {pagina} salvos localmente em {nome_arquivo}")  
         else:
             print(f"Nenhum dado encontrado na página {pagina}.")
     
--- scripts/web_scraper.py
+++ scripts/web_scraper.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_autoinstaller
 import time
 
 def coletar_dados(driver, url):
@@ -25,9 +24,26 @@ def coletar_dados(driver, url):
         
         # As classes de estrelas são 'star-rating One', 'star-rating Two', etc.
         estrelas = estrelas_classe.split()[-1]
-        dados.append(f"{titulo} - {preco} - {estrelas}")
+        # Mapeia a classe de estrelas para um número
+        estrelas_dict = {
+            'One': 1,
+            'Two': 2,
+            'Three': 3,
+            'Four': 4,
+            'Five': 5
+        }
+        estrelas_numero = estrelas_dict.get(estrelas, 0)
+        
+        # Adiciona o título, o preço e as estrelas à lista de dados
+        dados.append({
+            "title": titulo,
+            "price": preco,
+            "stars": estrelas_numero
+        })
     
     return dados
 
 def iniciar_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    # Instala automaticamente o ChromeDriver compatível
+    chromedriver_autoinstaller.install()
+    return webdriver.Chrome()
